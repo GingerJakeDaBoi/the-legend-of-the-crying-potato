0a09a54f59898efef3bc43026fbc4764f8be5617
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-07-30 01:57:32 -0400

    Draw PotatoMain.png to the window

--- src/main/java/org/gingerjake/potatogame/StartScreen.java
+++ src/main/java/org/gingerjake/potatogame/StartScreen.java
@@ -1,8 +1,11 @@
 package org.gingerjake.potatogame;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class StartScreen extends GameState {
+    Image PotatoMain = new ImageIcon("Assets/Potato/PotatoMain.png").getImage();
+
     public StartScreen(GameStateManager gsm) {
         super(gsm);
     }
@@ -11,13 +14,17 @@ public class StartScreen extends GameState {
 
     }
 
+
     public void draw(Graphics g) {
-        g.setColor(new Color(0,0,0,0));
+        g.setColor(new Color(0, 0, 0, 0));
         g.fillRect(0, 0, GamePanel.width, GamePanel.height);
 
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 50));
         g.drawString("Press Enter to start", 0, (int) (GamePanel.height * .945));
+
+
+        g.drawImage(PotatoMain, (int) (GamePanel.width * .65), (int) (GamePanel.height * .5), 256, 256, null);
 
     }
 
