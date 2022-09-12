5c3ea2651fa62fe2a6184a7d7e79519d06a8061f
GingerJake <uniquereddy90@gmail.com>
2022-09-12 11:12:04 -0400

    disable savesystem again probs gonna remake it

--- src/main/java/org/gingerjake/potatogame/Controls.java
+++ src/main/java/org/gingerjake/potatogame/Controls.java
@@ -14,6 +14,7 @@ public class Controls {
             }
         }
     }
+
     public static void menuDown() {
         if (PauseMenu.paused) {
             if (PauseMenu.Selection < 1) {
@@ -30,6 +31,7 @@ public class Controls {
             }
         }
     }
+
     public static void moveDown() {
         if (PlayerController.enabled) {
             if (!PlayerController.downing) {
@@ -38,12 +40,14 @@ public class Controls {
             }
         }
     }
+
     public static void moveLeft() {
         if (!PlayerController.lefting) {
             PlayerController.lefting = true;
             new Thread(PlayerController::moveLeft).start();
         }
     }
+
     public static void moveRight() {
         if (!PlayerController.righting) {
             PlayerController.righting = true;
@@ -56,35 +60,42 @@ public class Controls {
             new Thread(Fist::up).start();
         }
     }
+
     public static void fistDown() {
         if (!Fist.visible) {
             new Thread(Fist::down).start();
         }
     }
+
     public static void fistLeft() {
         if (!Fist.visible) {
             new Thread(Fist::left).start();
         }
     }
+
     public static void fistRight() {
         if (!Fist.visible) {
             new Thread(Fist::right).start();
         }
     }
+
     public static void cancelFist() {
         Fist.visible = false;
     }
 
     public static void pause() {
-        if (!PauseMenu.paused) {
-            PauseMenu.paused = true;
-            if (EndScreen.gameEnded) {
-                System.out.println("Thanks for playing!");
-                System.exit(0);
+        if (GamePanel.gameStarted) {
+            if (!PauseMenu.paused) {
+                PauseMenu.paused = true;
+                if (EndScreen.gameEnded) {
+                    System.out.println("Thanks for playing!");
+                    System.exit(0);
+                }
+                GameStateManager.pause();
             }
-            GameStateManager.pause();
         }
     }
+
     public static void resume() {
         if (PauseMenu.paused) {
             PauseMenu.paused = false;
@@ -96,17 +107,20 @@ public class Controls {
             GameStateManager.resume();
         }
     }
+
     public static void startGame() {
         if (!GamePanel.gameStarted) {
             GameStateManager.setState(new BigLevelTest());
         }
     }
+
     public static void endGame() {
         if (EndScreen.gameEnded) {
             System.out.println("Thanks for playing!");
             System.exit(0);
         }
     }
+
     public static void select() {
         if (PauseMenu.paused) {
             if (PauseMenu.Selection == 0) {
