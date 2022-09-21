9548d133572ed1c60ea3683e6b1ffe7a8d404de8
GingerJake <uniquereddy90@gmail.com>
2022-09-21 09:53:19 -0400

    more map work

--- src/main/java/org/gingerjake/potatogame/Levels/SpeedGauntlet/Right/SpeedHorizontal2.java
+++ src/main/java/org/gingerjake/potatogame/Levels/SpeedGauntlet/Right/SpeedHorizontal2.java
@@ -1,4 +1,4 @@
-package org.gingerjake.potatogame.Levels.SpeedGauntlet;
+package org.gingerjake.potatogame.Levels.SpeedGauntlet.Right;
 
 import org.gingerjake.potatogame.Actors.Enemies.Enemy;
 import org.gingerjake.potatogame.Actors.Player.Attacks.Fist;
@@ -7,68 +7,48 @@ import org.gingerjake.potatogame.Controls;
 import org.gingerjake.potatogame.GamePanel;
 import org.gingerjake.potatogame.GameState;
 import org.gingerjake.potatogame.GameStateManager;
-import org.gingerjake.potatogame.Levels.SpeedGauntlet.Right.SpeedHorizontal2;
+import org.gingerjake.potatogame.Levels.SpeedGauntlet.Up.SpeedHorizontal1;
 import org.gingerjake.potatogame.Levels.SpeedGauntlet.Up.SpeedVertical1;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Objects;
 
-public class SpeedFork extends GameState {
+public class SpeedHorizontal2 extends GameState {
     Image playerHeart = new ImageIcon("Assets/GUI/Heart.png").getImage();
     Image playerHeartBroken = new ImageIcon("Assets/GUI/HeartBroken.png").getImage();
     Image chaser = new ImageIcon("Assets/Dummy/Red.png").getImage();
-    Image background = new ImageIcon("Assets/SpeedGauntlet/PathFork.png").getImage();
-    Image nextUpLvl = new ImageIcon("Assets/SpeedGauntlet/Vertical.png").getImage();
-    Image nextRightLvl = new ImageIcon("Assets/SpeedGauntlet/Horizontal.png").getImage();
-    Image debugBox = new ImageIcon("Assets/Dummy/Green.png").getImage();
+    Image background = new ImageIcon("Assets/SpeedGauntlet/Horizontal.png").getImage();
+    Image nextLvl = new ImageIcon("Assets/SpeedGauntlet/Vertical.png").getImage();
 
     Image fist;
     boolean finished;
     boolean switching;
-    boolean switchUp;
-    boolean switchRight;
-    int nextUpLvlX = 0;
-    int nextUpLvlY = -861;
-    int nextRightLvlX = GamePanel.width;
-    int nextRightLvlY = 0;
+    int nextLvlX = 0;
+    int nextLvlY = -861;
     int currentLvlX = 0;
     int currentLvlY = 0;
     //Hitbox for the top part of the bottom left side
-    int hitbox1aX = 780;
-    int hitbox1aY = 621;
-    int hitbox1aW = GamePanel.width;
-    int hitbox1aH = 1;
-    //now the left part
-    int hitbox1bX = 773;
-    int hitbox1bY = 621;
-    int hitbox1bW = 1;
-    int hitbox1bH = GamePanel.height;
+    int hitbox1X = 0;
+    int hitbox1Y = 220;
+    int hitbox1W = GamePanel.width;
+    int hitbox1H = 1;
     //Hitbox for the bottom part of the top left side
-    int hitbox2aX = 790;
-    int hitbox2aY = 225;
-    int hitbox2aW = GamePanel.width;
-    int hitbox2aH = 1;
-    //now the left part
-    int hitbox2bX = 773;
-    int hitbox2bY = 0;
-    int hitbox2bW = 1;
-    int hitbox2bH = 205;
-    //hitbox for the right side
-    int hitbox3X = 0;
-    int hitbox3Y = 0;
-    int hitbox3W = 276;
-    int hitbox3H = 861;
+    int hitbox2X = 0;
+    int hitbox2Y = 622;
+    int hitbox2W = GamePanel.width;
+    int hitbox2H = 1;
 
 
-    public SpeedFork() {
+
+    public SpeedHorizontal2() {
         super(gsm);
         Enemy.disable();
 
-        PlayerController.x = 400;
-        PlayerController.y = GamePanel.height - PlayerController.height;
+        PlayerController.x = 115;
+        PlayerController.y = 300;
         PlayerController.enable();
-        Enemy.spawn(450, 50, 64, 64, 2, 3);
+        Enemy.spawn(1334,300,64,64,2,3);
     }
 
     @Override
@@ -79,8 +59,7 @@ public class SpeedFork extends GameState {
     @Override
     public void draw(Graphics g) {
         g.drawImage(background, currentLvlX, currentLvlY, GamePanel.width, GamePanel.height, null);
-        g.drawImage(nextUpLvl, nextUpLvlX, nextUpLvlY, 1584, 861, null);
-        g.drawImage(nextRightLvl, nextRightLvlX, nextRightLvlY, GamePanel.width, GamePanel.height, null);
+        g.drawImage(nextLvl,nextLvlX, nextLvlY, 1584, 861, null);
 
 //        g.drawImage(debugBox,PlayerController.x,PlayerController.y,PlayerController.width,PlayerController.height,null);
         g.drawImage(PlayerController.playerAsset, PlayerController.x, PlayerController.y, PlayerController.width, PlayerController.height, null);
@@ -149,59 +128,28 @@ public class SpeedFork extends GameState {
             PlayerController.y = 0;
         }
 
-        if (PlayerController.x + PlayerController.width > hitbox1aX && PlayerController.x < hitbox1aX + hitbox1aW && PlayerController.y + PlayerController.height > hitbox1aY && PlayerController.y < hitbox1aY + hitbox1aH) {
-            PlayerController.y = 621 - PlayerController.height;
+        if (PlayerController.x + PlayerController.width > hitbox1X && PlayerController.x < hitbox1X + hitbox1W && PlayerController.y + PlayerController.height > hitbox1Y && PlayerController.y < hitbox1Y + hitbox1H) {
+            PlayerController.y = 220;
         }
-        if (PlayerController.x + PlayerController.width > hitbox1bX && PlayerController.x < hitbox1bX + hitbox1bW && PlayerController.y + PlayerController.height > hitbox1bY && PlayerController.y < hitbox1bY + hitbox1bH) {
-            PlayerController.x = 773 - PlayerController.width;
-        }
-        if (PlayerController.x + PlayerController.width > hitbox2aX && PlayerController.x < hitbox2aX + hitbox2aW && PlayerController.y + PlayerController.height > hitbox2aY && PlayerController.y < hitbox2aY + hitbox2aH) {
-            PlayerController.y = 225;
-        }
-        if (PlayerController.x + PlayerController.width > hitbox2bX && PlayerController.x < hitbox2bX + hitbox2bW && PlayerController.y + PlayerController.height > hitbox2bY && PlayerController.y < hitbox2bY + hitbox2bH) {
-            PlayerController.x = 773 - PlayerController.width;
-        }
-        if (PlayerController.x + PlayerController.width > hitbox3X && PlayerController.x < hitbox3X + hitbox3W && PlayerController.y + PlayerController.height > hitbox3Y && PlayerController.y < hitbox3Y + hitbox3H) {
-            PlayerController.x = hitbox3W;
+        if (PlayerController.x + PlayerController.width > hitbox2X && PlayerController.x < hitbox2X + hitbox2W && PlayerController.y + PlayerController.height > hitbox2Y && PlayerController.y < hitbox2Y + hitbox2H) {
+            PlayerController.y = 622 - PlayerController.height;
         }
 
-        if (!Enemy.enabled) {
+        if(!Enemy.enabled) {
             finished = true;
         }
-        if (finished) {
-            if (PlayerController.y == 0) {
+        if(finished) {
+            if(PlayerController.x == GamePanel.width - PlayerController.width) {
                 switching = true;
-                switchUp = true;
-                switchRight = false;
             }
-            if (PlayerController.x == GamePanel.width - PlayerController.width) {
-                switching = true;
-                switchUp = false;
-                switchRight = true;
-            }
-            if (switching) {
+            if(switching) {
                 PlayerController.playerAsset = new ImageIcon((String) null).getImage();
-                if (switchUp) {
-                    PlayerController.y -= 5;
-
-                    if (nextUpLvlY < 0) {
-                        currentLvlY += 3;
-                        nextUpLvlY += 3;
-                    } else {
-                        PlayerController.playerAsset = new ImageIcon("Assets/Potato/NewMainL.png").getImage();
-                        GameStateManager.setState(new SpeedVertical1());
-                    }
-                }
-                if (switchRight) {
-                    PlayerController.x += 5;
-
-                    if (nextRightLvlX > 0) {
-                        currentLvlX -= 3;
-                        nextRightLvlX -= 3;
-                    } else {
-                        PlayerController.playerAsset = new ImageIcon("Assets/Potato/NewMainL.png").getImage();
-                        GameStateManager.setState(new SpeedHorizontal2());
-                    }
+                if(nextLvlX > 0) {
+                    currentLvlX -= 3;
+                    nextLvlX -= 3;
+                } else {
+                    PlayerController.playerAsset = new ImageIcon("Assets/Potato/NewMainL.png").getImage();
+                    GameStateManager.setState(new SpeedHorizontal1());
                 }
             }
         }
